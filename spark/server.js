00ce8e58f7048e5220181ab1c127b9fd0d219c63
gulfammureed <71508543+gulfammureed@users.noreply.github.com>
2020-09-22 11:54:27 -0400

    Update server.js

--- spark/server.js
+++ spark/server.js
@@ -31,9 +31,9 @@ app.use(express.json());
 //Upon closure the webhook is removed from spark
 
 //Insert your values here
-const sparkAccessToken ="Place your spark personal access token here";
+const sparkAccessToken ="MGQwZTEwYmMtN2JhZC00ZDdmLWIyYjQtZGUxODI3OGQ1M2QwZDQ0ODcxMjAtY2Vh_PF84_0d882151-70b4-4264-a09c-4a599e4494b1";
 const targetUrl = 'Place you servers URL here';
-const projectId = 'Place your dialogflow projectId here';
+const projectId = 'flash-adapter-289912';
 
 const sessionClient = new dialogflowSessionClient(projectId);
 
